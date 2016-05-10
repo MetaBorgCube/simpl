6053cbed7b2d68bbe2e76d8b60d25c0f108d8d08
Vlad Vergu <v.vergu@gmail.com>
2016-05-10 15:16:02 +0300

    Add native operators

---
+++ simpl.interpreter/src/main/java/simpl/interpreter/natives/addI_2.java
@@ -0,0 +1,28 @@
+package simpl.interpreter.natives;
+
+import org.metaborg.meta.lang.dynsem.interpreter.nodes.building.TermBuild;
+
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.NodeChildren;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.source.SourceSection;
+
+@NodeChildren({ @NodeChild(value = "left", type = TermBuild.class),
+		@NodeChild(value = "right", type = TermBuild.class) })
+public abstract class addI_2 extends TermBuild {
+
+	public addI_2(SourceSection source) {
+		super(source);
+	}
+
+	@Specialization
+	public int doInt(int left, int right) {
+		return left + right;
+	}
+
+	public static TermBuild create(SourceSection source, TermBuild left,
+			TermBuild right) {
+		return addI_2NodeGen.create(source, left, right);
+	}
+
+}
